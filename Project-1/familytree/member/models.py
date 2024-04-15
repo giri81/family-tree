7602da769dd17510971a4eb17da4bf8d9ec4fdc0
giri81 <sd.girish@gmail.com>
2024-04-15 16:46:00 +0530

    Lecture 182, Locial Clone Part 7

--- Project-1/familytree/member/models.py
+++ Project-1/familytree/member/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
 
-# Create your models here.
+# pip install misaka
+import misaka
+
+from community.models import Community
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class Member(models.Model):
+    user = models.ForeignKey(User, related_name="member",on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    community = models.ForeignKey(Community, related_name="member",null=True, blank=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.message
+
+    def save(self, *args, **kwargs):
+        self.message_html = misaka.html(self.message)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse(
+            "member:single",
+            kwargs={
+                "username": self.user.username,
+                "pk": self.pk
+            }
+        )
+
+    class Meta:
+        ordering = ["-created_at"]
+        unique_together = ["user", "message"]
